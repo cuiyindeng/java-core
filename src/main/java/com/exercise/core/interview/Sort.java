e5e04c41a9cb46a0ed637862a64c570e9156e1d4
cuiyindeng <543770144@qq.com>
2019-09-05 13:27:57 +0800

    update

---
+++ src/main/java/com/exercise/core/interview/Sort.java
@@ -0,0 +1,26 @@
+package com.exercise.core.interview;
+
+import java.util.Arrays;
+
+public class Sort {
+
+    public static void main(String[] args) {
+        Integer[] arr = {2, 1, 4, 5, 3};
+        System.out.println(Arrays.asList(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.asList(arr));
+
+    }
+
+    public static void bubbleSort(Integer[] arr ) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length -1 - i; j++){
+                if(arr[j + 1] < arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
